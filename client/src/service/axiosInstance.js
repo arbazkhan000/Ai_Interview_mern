8afa94545c7ea5fb16e1a1c4b9bdee0f069ba561
Arbazx01 <mdarbaz9675@gmail.com>
2025-05-31 20:49:33 +0530

    change url

--- client/src/service/axiosInstance.js
+++ client/src/service/axiosInstance.js
@@ -2,10 +2,12 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: "https://ai-interview-backend-nu.vercel.app/api/v1",
+    baseURL: "https://ai-interview-backend-nu.vercel.app/api/v1/",
     headers: {
         "Content-Type": "application/json",
     },
 });
 
 export default axiosInstance;
+
+
